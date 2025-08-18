f65939a6dd487666d72bbea09685c3b1b73050ed
Vladimir Tolstov <tolstov@gmail.com>
2025-08-18 11:09:22 +0700

    fixed static resources

--- src/main/java/org/weather/app/config/SpringConfig.java
+++ src/main/java/org/weather/app/config/SpringConfig.java
@@ -14,6 +14,7 @@ import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -154,5 +155,11 @@ public class SpringConfig implements WebMvcConfigurer {
                 .load();
         flyway.migrate();
         return flyway;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 }
