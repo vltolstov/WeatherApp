553e098525f9e0dcd1b53e37e5ccec5af8147f46
Vladimir Tolstov <tolstov@gmail.com>
2025-08-26 14:11:02 +0700

    added dao layer for user

---
+++ src/main/java/org/weather/app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.weather.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.weather.app.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
