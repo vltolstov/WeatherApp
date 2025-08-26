5aceb38817833c7f324b4cccc54bdbaf27a8d022
Vladimir Tolstov <tolstov@gmail.com>
2025-08-26 14:08:39 +0700

    moved to controller catalog

--- src/main/java/org/weather/app/controller/HelloController.java
+++ src/main/java/org/weather/app/controller/HelloController.java
@@ -1,4 +1,4 @@
-package org.weather.app.controllers;
+package org.weather.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/org/weather/app/controller/RegistrationController.java
+++ src/main/java/org/weather/app/controller/RegistrationController.java
@@ -1,4 +1,4 @@
-package org.weather.app.controllers;
+package org.weather.app.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -7,26 +7,33 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.weather.app.dto.UserRegistrationDto;
+import org.weather.app.dto.UserRegistrationRequest;
+import org.weather.app.service.UserService;
 
 @Controller
 public class RegistrationController {
 
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/registration")
     public String registration(Model model) {
-        model.addAttribute("userRegistrationDto", new UserRegistrationDto());
+        model.addAttribute("userRegistrationDto", new UserRegistrationRequest());
         return "pages/registration";
     }
 
     @PostMapping("/registration")
-    public String processRegistration(@Valid @ModelAttribute("userRegistrationDto") UserRegistrationDto userRegistrationDto, BindingResult result, Model model) {
+    public String processRegistration(@Valid @ModelAttribute("userRegistrationDto") UserRegistrationRequest userRegistrationDto, BindingResult result) {
 
         if (result.hasErrors()) {
             return "pages/registration";
         }
 
-        //сохранение пользователя
+        userService.registerUser(userRegistrationDto);
 
-        return "redirect:/index";
+        return "redirect:/login";
     }
 }
