e3517022626ffddc46cb979c5aab43baccadc6b2
Vladimir Tolstov <tolstov@gmail.com>
2025-10-01 09:41:16 +0700

    fixed mapper

--- src/main/java/org/weather/app/mapper/UserMapper.java
+++ src/main/java/org/weather/app/mapper/UserMapper.java
@@ -1,14 +1,11 @@
 package org.weather.app.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import org.weather.app.dto.UserRegistrationRequest;
 import org.weather.app.model.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     User toEntity(UserRegistrationRequest userRegistrationRequest);
 
