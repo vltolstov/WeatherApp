cbcbeb5ed623b8e7ee35dad11f256d9ba0718dad
Vladimir Tolstov <tolstov@gmail.com>
2025-10-02 16:59:58 +0700

    updated index page

--- src/main/java/org/weather/app/controller/IndexController.java
+++ src/main/java/org/weather/app/controller/IndexController.java
@@ -1,25 +1,45 @@
 package org.weather.app.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.weather.app.dto.SearchRequest;
+import org.weather.app.dto.WeatherResponseDto;
+import org.weather.app.model.Location;
+import org.weather.app.model.User;
+import org.weather.app.repository.LocationRepository;
 import org.weather.app.service.WeatherService;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
-    private WeatherService weatherService;
-
-    public IndexController(WeatherService weatherService) {
-        this.weatherService = weatherService;
-    }
+    private final WeatherService weatherService;
+    private final LocationRepository locationRepository;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, HttpServletRequest httpServletRequest) {
         model.addAttribute("searchRequest", new SearchRequest());
 
-        weatherService.getWeatherByCity("Novosibirsk");
+        User user = (User) httpServletRequest.getAttribute("user");
+
+        boolean exists = locationRepository.existsByUser(user);
+
+        if (!exists) {
+            model.addAttribute("locationsNotFound", "Города отсутствуют. Добавьте через поиск");
+            return "pages/index";
+        }
+
+        List<Location> locations = locationRepository.findByUser(user);
+        List<WeatherResponseDto> userWeatherList = locations.stream()
+                .map(location -> weatherService.getWeatherByCoordinates(location.getLongitude(), location.getLatitude()))
+                .toList();
+        model.addAttribute("userWeatherList", userWeatherList);
+
         return "pages/index";
     }
 }
