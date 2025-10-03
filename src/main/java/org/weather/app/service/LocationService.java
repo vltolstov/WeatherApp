d5b09f29e2ebbe28d6d45e7e55c2dea1cdc1a1c0
Vladimir Tolstov <tolstov@gmail.com>
2025-10-03 11:34:36 +0700

    added location service

---
+++ src/main/java/org/weather/app/service/LocationService.java
@@ -0,0 +1,52 @@
+package org.weather.app.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.weather.app.dto.LocationRequest;
+import org.weather.app.dto.LocationResponse;
+import org.weather.app.mapper.LocationMapper;
+import org.weather.app.model.Location;
+import org.weather.app.model.User;
+import org.weather.app.repository.LocationRepository;
+
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
+
+    @Transactional
+    public LocationResponse addLocation(User user, LocationRequest locationRequest) {
+        boolean exists = locationRepository.existsByUserAndLongitudeAndLatitude(
+                user,
+                locationRequest.longitude(),
+                locationRequest.latitude()
+        );
+
+        if (exists) {
+            return new LocationResponse(false, "Город уже добавлен");
+        }
+
+        Location location = locationMapper.toEntity(locationRequest, user);
+        locationRepository.save(location);
+
+        return new LocationResponse(true, "Добавлен");
+    }
+
+    @Transactional
+    public LocationResponse deleteLocation(User user, LocationRequest locationRequest) {
+        long deletedCount = locationRepository.deleteByUserAndLongitudeAndLatitude(
+                user,
+                locationRequest.longitude(),
+                locationRequest.latitude()
+        );
+
+        if (deletedCount == 0) {
+            return new LocationResponse(false, "Город не найден");
+        }
+
+        return new LocationResponse(true, "Город удален");
+    }
+}
